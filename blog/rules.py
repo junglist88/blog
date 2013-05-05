b1c57bb77d2200e83068ba93facc006ed3e57c18
Corey Burmeister <burmeister.corey@gmail.com>
2013-05-04 23:19:22 -0700

    enable edit

--- blog/rules.py
+++ blog/rules.py
@@ -4,3 +4,4 @@ from werkzeug.routing import Rule
 app.url_map.add(Rule('/', endpoint='index'))
 app.url_map.add(Rule('/write', endpoint='write'))
 app.url_map.add(Rule('/<url>', endpoint='read'))
+app.url_map.add(Rule('/edit/<id>', endpoint='edit'))
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from flask import request, redirect, url_for, render_template, abort
 from blog import app, rules
 from models.entry import Entry
+from markdown import markdown
 
 
 @app.endpoint('index')
@@ -9,15 +10,15 @@ def index():
     return render_template('index.html', entries=entries)
 
 @app.endpoint('read')
-def write(url):
+def read(url):
     entry = Entry.objects(url=url).first()
     if not entry:
         abort(404)
+    entry.text = markdown(entry.text, ['codehilite'])
     return render_template('read.html', entry=entry)
 
 @app.endpoint('write')
 def write():
-
     if request.method == 'POST':
         entry = Entry(title=request.form['title'],
                 url=request.form['url'],
@@ -26,3 +27,17 @@ def write():
         return redirect(url_for('index'))
 
     return render_template('write.html')
+
+@app.endpoint('edit')
+def edit(id):
+    entry = Entry.objects(id=id).first()
+    if not entry:
+        abort(404)
+    if request.method == 'POST':
+        entry.title = request.form['title']
+        entry.url = request.form['url']
+        entry.text = request.form['text']
+        entry.save()
+        return redirect(url_for('read', url=entry.url))
+
+    return render_template('edit.html', entry=entry)

cfd1ff030f99395ec122c791ba5dff3b8013ccd2
Corey Burmeister <burmeister.corey@gmail.com>
2013-05-05 11:34:35 -0700

    custom 404 baby

--- blog/views.py
+++ blog/views.py
@@ -63,3 +63,7 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
